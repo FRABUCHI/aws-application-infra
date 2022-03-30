8bc5da673520801530ef0defc7b8e1b12ae382b5
FRAFUCHI <alphasuno214@gmail.com>
2022-03-30 13:53:29 +0900

    refact: change beanstalk env name

--- module/beanstalk/beanstalk-environment.tf
+++ module/beanstalk/beanstalk-environment.tf
@@ -1,5 +1,5 @@
-resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "docker-linux"
+resource "aws_elastic_beanstalk_environment" "docker" {
+  name                = "docker"
   application         = aws_elastic_beanstalk_application.sample_api_server.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.12 running Docker"
 }
