7ee9fb1b4b2016354dd6efd8c425d0b6bc69876e
FRAFUCHI <alphasuno214@gmail.com>
2022-03-30 14:00:02 +0900

    feat: Add beanstalk evn network setting

--- module/beanstalk/beanstalk-environment.tf
+++ module/beanstalk/beanstalk-environment.tf
@@ -2,4 +2,22 @@ resource "aws_elastic_beanstalk_environment" "docker" {
   name                = "docker"
   application         = aws_elastic_beanstalk_application.sample_api_server.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.12 running Docker"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "${module.global_variables.this_network.outputs.this_vpc_id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${module.global_variables.this_network.outputs.this_pri_sbnt_a_id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${module.global_variables.this_network.outputs.this_pri_sbnt_c_id}"
+  }
 }
